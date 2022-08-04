467647a786773f3131dc3a0811716430918d4215
Mohammed Taitai <mohammedtaitai@outlook.com>
2022-08-04 14:04:47 -0400

    commit tree b68bb71938991d16047e39d077a4accf208796e4

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import NavBar from "./NavBar";
+import About from "./About";
+import Home from "./Home";
+import {username, city, image} from "../data/user";
 
 function App() {
   return (
